25c3b3d07d50e30493342e6424be45069a3a1694
qellzXan <49795679+qellzXan@users.noreply.github.com>
2022-03-22 12:31:25 +0300

    Add files via upload

---
+++ dllmain_inject.cpp
@@ -0,0 +1,30 @@
+﻿
+#include <Windows.h>
+
+void main()
+{
+
+     AllocConsole();
+    freopen("CONOUT$", "w", stdout);
+    std::cout << "Hello world" << std::endl;
+}
+
+
+BOOL APIENTRY DllMain(HMODULE hModule,
+    DWORD  ul_reason_for_call,
+    LPVOID lpReserved)
+{
+    switch (ul_reason_for_call)
+    {
+    case DLL_PROCESS_ATTACH: {
+        DisableThreadLibraryCalls(hModule);
+        CreateThread(NULL, NULL, (LPTHREAD_START_ROUTINE)main, NULL, NULL, NULL);
+    }
+    case DLL_THREAD_ATTACH:
+    case DLL_THREAD_DETACH:
+    case DLL_PROCESS_DETACH:
+        break;
+    }
+    return TRUE;
+}
+
